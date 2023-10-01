5d3a517db39beb46db43b1d11c9c62e9ba6a8a1f
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-01 11:16:49 +0800

    ref: move the logic to generate the list of  fsvids into it's own utils function
    This is so it can be reused by the logic that writes the input file for
    the final vid

--- skim/cmd/web/handlers/getFSVidsHandler.go
+++ skim/cmd/web/handlers/getFSVidsHandler.go
@@ -2,14 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/gregidonut/VEWorkflowAutomation/skim/cmd/web/fsvid"
 	"github.com/gregidonut/VEWorkflowAutomation/skim/cmd/web/paths"
+	"github.com/gregidonut/VEWorkflowAutomation/skim/cmd/web/utils"
 	"net/http"
 	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 )
 
 func GetFSVid(w http.ResponseWriter, r *http.Request) {
@@ -19,33 +15,11 @@ func GetFSVid(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var fsVids []fsvid.FSVid
-	files, err := os.ReadDir(paths.FSVIDS_REL_PATH)
+	fsVids, err := utils.GenerateFsVidList()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	for _, f := range files {
-		if f.IsDir() {
-			continue
-		}
-		scriptBasePath := strings.TrimSuffix(filepath.Base(f.Name()), ".mp4") + ".txt"
-		fsVid, err := fsvid.NewFSVid(map[string]string{
-			"vPath":      fmt.Sprintf("%s/%s", paths.FSVIDS_REL_PATH, filepath.Base(f.Name())),
-			"scriptPath": fmt.Sprintf("%s/%s", paths.RAW_COMMIT_VIDS_REL_PATH, scriptBasePath),
-		})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fsVids = append(fsVids, *fsVid)
-	}
-
-	sort.Slice(fsVids, func(i, j int) bool {
-		return fsVids[i].VBasePath < fsVids[j].VBasePath
-	})
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(fsVids)
---
+++ skim/cmd/web/utils/generateFSVidList.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/gregidonut/VEWorkflowAutomation/skim/cmd/web/fsvid"
+	"github.com/gregidonut/VEWorkflowAutomation/skim/cmd/web/paths"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func GenerateFsVidList() ([]fsvid.FSVid, error) {
+	files, err := os.ReadDir(paths.FSVIDS_REL_PATH)
+	if err != nil {
+		return nil, nil
+	}
+
+	var fsVids []fsvid.FSVid
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		scriptBasePath := strings.TrimSuffix(filepath.Base(f.Name()), ".mp4") + ".txt"
+		fsVid, err := fsvid.NewFSVid(map[string]string{
+			"vPath":      fmt.Sprintf("%s/%s", paths.FSVIDS_REL_PATH, filepath.Base(f.Name())),
+			"scriptPath": fmt.Sprintf("%s/%s", paths.RAW_COMMIT_VIDS_REL_PATH, scriptBasePath),
+		})
+		if err != nil {
+			return nil, nil
+		}
+
+		fsVids = append(fsVids, *fsVid)
+	}
+
+	sort.Slice(fsVids, func(i, j int) bool {
+		return fsVids[i].VBasePath < fsVids[j].VBasePath
+	})
+
+	return fsVids, nil
+}
