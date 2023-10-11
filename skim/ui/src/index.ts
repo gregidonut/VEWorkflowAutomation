5c64773b556d4a0b002237464a1388017605fdb7
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-11 23:07:25 +0800

    feat: add logic to show upload progress in client side
    This at-least shows progress the first step next commit should include progress in the copy phase

--- skim/ui/src/index.ts
+++ skim/ui/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello");
+const form = <HTMLFormElement>document.getElementById("upload-form");
+const uploadProgressElement = <HTMLProgressElement>document.getElementById("upload-progress");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.addEventListener("progress", (event) => {
+        if (event.lengthComputable) {
+            uploadProgressElement.value = (event.loaded / event.total) * 100;
+        }
+    });
+
+    xhr.open("POST", "/upload", true);
+
+    const formData = new FormData(form);
+    xhr.send(formData);
+});
+
