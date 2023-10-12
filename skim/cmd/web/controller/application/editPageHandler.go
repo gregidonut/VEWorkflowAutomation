3a7889925f128e8fe1ae023f88cf5b6052807de2
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-12 19:16:44 +0800

    feat: add initialTimeLineSection as a component
    Also added some intitial styles just to see what the design would look like

--- skim/cmd/web/controller/application/editPageHandler.go
+++ skim/cmd/web/controller/application/editPageHandler.go
@@ -1,9 +1,14 @@
 package application
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
+
+type templateData struct {
+	Video string
+}
 
 func (app *Application) EditPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/edit" {
@@ -14,6 +19,7 @@ func (app *Application) EditPage(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./skim/ui/html/base.gohtml",
 		"./skim/ui/html/pages/edit.gohtml",
+		"./skim/ui/html/partials/editPageComponents/initialTimeLineSection.gohtml",
 	}
 
 	ts, err := template.ParseFiles(files...)
@@ -24,7 +30,14 @@ func (app *Application) EditPage(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Clear-Site-Data", `"cache"`)
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	var data []templateData
+	for i := 0; i < 30; i++ {
+		data = append(data, templateData{
+			Video: fmt.Sprintf("ina_mo_%d.mp4", i),
+		})
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.catchHandlerErr(w, err, http.StatusInternalServerError)
 		return
